4a62d912e925615ae1c682eed45d37d28ea27733
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2021-05-21 01:43:34 +0200

    Implement Queen moves

--- app/src/main/java/com/github/zsoltk/rf1/model/piece/Queen.kt
+++ app/src/main/java/com/github/zsoltk/rf1/model/piece/Queen.kt
@@ -1,5 +1,7 @@
 package com.github.zsoltk.rf1.model.piece
 
+import com.github.zsoltk.rf1.model.game.GameState
+import com.github.zsoltk.rf1.model.notation.Position
 import com.github.zsoltk.rf1.model.piece.Set.*
 
 class Queen(override val set: Set) : Piece {
@@ -10,4 +12,7 @@ class Queen(override val set: Set) : Piece {
         WHITE -> "♕"
         BLACK -> "♛"
     }
+
+    override fun moves(gameState: GameState): List<Position> =
+        lineMoves(gameState, Rook.directions + Bishop.directions)
 }
