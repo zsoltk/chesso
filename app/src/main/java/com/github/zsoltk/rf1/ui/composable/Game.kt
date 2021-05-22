464288db7ba34e25edc686cf0ae2e96f4fa7b108
Zsolt Kocsi <zsolt.kocsi@corp.badoo.com>
2021-05-22 14:04:24 +0200

    Render next to move

--- app/src/main/java/com/github/zsoltk/rf1/ui/composable/Game.kt
+++ app/src/main/java/com/github/zsoltk/rf1/ui/composable/Game.kt
@@ -1,7 +1,19 @@
 package com.github.zsoltk.rf1.ui.composable
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.zsoltk.rf1.model.game.Game
 import com.github.zsoltk.rf1.model.game.UiState
 import com.github.zsoltk.rf1.model.notation.Position.*
@@ -9,13 +21,31 @@ import com.github.zsoltk.rf1.ui.Rf1Theme
 
 @Composable
 fun Game(game: Game = Game(), uiState: UiState = UiState()) {
-    Board(
-        gameState = game.states.last(),
-        uiState = uiState,
-        onMove = { from, to ->
-            game.applyMove(from, to)
-        }
-    )
+    Column {
+        ToMove(game)
+        Board(
+            gameState = game.states.last(),
+            uiState = uiState,
+            onMove = { from, to ->
+                game.applyMove(from, to)
+            }
+        )
+    }
+}
+
+@Composable
+private fun ToMove(game: Game) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.primaryVariant)
+    ) {
+        Text(
+            text = "${game.states.last().toMove} to move",
+            modifier = Modifier.padding(16.dp),
+            color = MaterialTheme.colors.onPrimary
+        )
+    }
 }
 
 @Preview(showBackground = true)
