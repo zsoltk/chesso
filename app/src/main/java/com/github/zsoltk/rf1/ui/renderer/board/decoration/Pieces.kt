6cc51ef470f279f5f826655e8a95d4d3fcd3150b
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2021-06-13 14:30:45 +0200

    Snap animation when board is flipped

--- app/src/main/java/com/github/zsoltk/rf1/ui/renderer/board/decoration/Pieces.kt
+++ app/src/main/java/com/github/zsoltk/rf1/ui/renderer/board/decoration/Pieces.kt
@@ -1,16 +1,19 @@
 package com.github.zsoltk.rf1.ui.renderer.board.decoration
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.key
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -23,8 +26,8 @@ import com.github.zsoltk.rf1.model.piece.Rook
 import com.github.zsoltk.rf1.ui.composable.BoardPreview
 import com.github.zsoltk.rf1.ui.composable.toModifier
 import com.github.zsoltk.rf1.ui.composable.toOffset
-import com.github.zsoltk.rf1.ui.renderer.board.BoardRenderProperties
 import com.github.zsoltk.rf1.ui.renderer.board.BoardDecoration
+import com.github.zsoltk.rf1.ui.renderer.board.BoardRenderProperties
 
 object Pieces : BoardDecoration {
 
@@ -32,17 +35,27 @@ object Pieces : BoardDecoration {
     override fun render(properties: BoardRenderProperties) {
         properties.toState.board.pieces.forEach { (toPosition, piece) ->
             key(piece) {
-                val targetCoordinate = toPosition.toCoordinate(properties.isFlipped)
-                val targetOffset = targetCoordinate.toOffset(properties.squareSize)
-                val offset by animateOffsetAsState(
-                    targetValue = targetOffset,
-                    animationSpec = tween(100, easing = LinearEasing)
-                )
+                val fromPosition = properties.fromState.board.find(piece)?.position
+                val currentOffset = fromPosition
+                    ?.toCoordinate(properties.isFlipped)
+                    ?.toOffset(properties.squareSize)
+
+                val targetOffset = toPosition
+                    .toCoordinate(properties.isFlipped)
+                    .toOffset(properties.squareSize)
+
+                val offset = remember { Animatable(currentOffset ?: targetOffset, Offset.VectorConverter) }
+                LaunchedEffect(targetOffset) {
+                    offset.animateTo(targetOffset, tween(100, easing = LinearEasing))
+                }
+                LaunchedEffect(properties.isFlipped) {
+                    offset.snapTo(targetOffset)
+                }
 
                 Piece(
                     piece = piece,
                     squareSize = properties.squareSize,
-                    modifier = offset.toModifier()
+                    modifier = offset.value.toModifier()
                 )
             }
         }
