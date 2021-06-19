0d1a9d4ed812cd0d970457e506865e3a343a5a58
Zsolt Kocsi <zsolt.kocsi@team.bumble.com>
2021-06-19 18:36:11 +0200

    Format check mate count as percentage

--- app/src/main/java/com/github/zsoltk/rf1/model/dataviz/impl/CheckmateCount.kt
+++ app/src/main/java/com/github/zsoltk/rf1/model/dataviz/impl/CheckmateCount.kt
@@ -1,7 +1,70 @@
 package com.github.zsoltk.rf1.model.dataviz.impl
 
 import com.github.zsoltk.rf1.model.board.Position
-import com.github.zsoltk.rf1.model.board.Position.*
+import com.github.zsoltk.rf1.model.board.Position.a1
+import com.github.zsoltk.rf1.model.board.Position.a2
+import com.github.zsoltk.rf1.model.board.Position.a3
+import com.github.zsoltk.rf1.model.board.Position.a4
+import com.github.zsoltk.rf1.model.board.Position.a5
+import com.github.zsoltk.rf1.model.board.Position.a6
+import com.github.zsoltk.rf1.model.board.Position.a7
+import com.github.zsoltk.rf1.model.board.Position.a8
+import com.github.zsoltk.rf1.model.board.Position.b1
+import com.github.zsoltk.rf1.model.board.Position.b2
+import com.github.zsoltk.rf1.model.board.Position.b3
+import com.github.zsoltk.rf1.model.board.Position.b4
+import com.github.zsoltk.rf1.model.board.Position.b5
+import com.github.zsoltk.rf1.model.board.Position.b6
+import com.github.zsoltk.rf1.model.board.Position.b7
+import com.github.zsoltk.rf1.model.board.Position.b8
+import com.github.zsoltk.rf1.model.board.Position.c1
+import com.github.zsoltk.rf1.model.board.Position.c2
+import com.github.zsoltk.rf1.model.board.Position.c3
+import com.github.zsoltk.rf1.model.board.Position.c4
+import com.github.zsoltk.rf1.model.board.Position.c5
+import com.github.zsoltk.rf1.model.board.Position.c6
+import com.github.zsoltk.rf1.model.board.Position.c7
+import com.github.zsoltk.rf1.model.board.Position.c8
+import com.github.zsoltk.rf1.model.board.Position.d1
+import com.github.zsoltk.rf1.model.board.Position.d2
+import com.github.zsoltk.rf1.model.board.Position.d3
+import com.github.zsoltk.rf1.model.board.Position.d4
+import com.github.zsoltk.rf1.model.board.Position.d5
+import com.github.zsoltk.rf1.model.board.Position.d6
+import com.github.zsoltk.rf1.model.board.Position.d7
+import com.github.zsoltk.rf1.model.board.Position.d8
+import com.github.zsoltk.rf1.model.board.Position.e1
+import com.github.zsoltk.rf1.model.board.Position.e2
+import com.github.zsoltk.rf1.model.board.Position.e3
+import com.github.zsoltk.rf1.model.board.Position.e4
+import com.github.zsoltk.rf1.model.board.Position.e5
+import com.github.zsoltk.rf1.model.board.Position.e6
+import com.github.zsoltk.rf1.model.board.Position.e7
+import com.github.zsoltk.rf1.model.board.Position.e8
+import com.github.zsoltk.rf1.model.board.Position.f1
+import com.github.zsoltk.rf1.model.board.Position.f2
+import com.github.zsoltk.rf1.model.board.Position.f3
+import com.github.zsoltk.rf1.model.board.Position.f4
+import com.github.zsoltk.rf1.model.board.Position.f5
+import com.github.zsoltk.rf1.model.board.Position.f6
+import com.github.zsoltk.rf1.model.board.Position.f7
+import com.github.zsoltk.rf1.model.board.Position.f8
+import com.github.zsoltk.rf1.model.board.Position.g1
+import com.github.zsoltk.rf1.model.board.Position.g2
+import com.github.zsoltk.rf1.model.board.Position.g3
+import com.github.zsoltk.rf1.model.board.Position.g4
+import com.github.zsoltk.rf1.model.board.Position.g5
+import com.github.zsoltk.rf1.model.board.Position.g6
+import com.github.zsoltk.rf1.model.board.Position.g7
+import com.github.zsoltk.rf1.model.board.Position.g8
+import com.github.zsoltk.rf1.model.board.Position.h1
+import com.github.zsoltk.rf1.model.board.Position.h2
+import com.github.zsoltk.rf1.model.board.Position.h3
+import com.github.zsoltk.rf1.model.board.Position.h4
+import com.github.zsoltk.rf1.model.board.Position.h5
+import com.github.zsoltk.rf1.model.board.Position.h6
+import com.github.zsoltk.rf1.model.board.Position.h7
+import com.github.zsoltk.rf1.model.board.Position.h8
 import com.github.zsoltk.rf1.model.dataviz.Datapoint
 import com.github.zsoltk.rf1.model.dataviz.DatasetVisualisation
 import com.github.zsoltk.rf1.model.game.state.GameSnapshotState
@@ -23,11 +86,13 @@ object CheckmateCount : DatasetVisualisation {
 
     override val maxValue: Int = 26745
 
+    private val total: Int = Position.values().sumOf { valueAt(it) }
+
     override fun dataPointAt(position: Position, state: GameSnapshotState): Datapoint {
         val value = valueAt(position)
         return Datapoint(
             value = value,
-            label = value.toString(),
+            label = "%.2f%%".format(100f * value / total),
             colorScale = silver_sand to amaranth_red,
         )
     }
